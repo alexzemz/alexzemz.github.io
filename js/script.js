db71f9116562d56825c9f99169cca5c590599fcb
alexzemz <48617088+alexzemz@users.noreply.github.com>
2020-12-23 17:08:51 +0000

    install node-sass

--- js/script.js
+++ js/script.js
@@ -18,16 +18,16 @@ for (let i = 0; i < themeDots.length; i++) {
 
 function setTheme(mode) {
   if (mode == "light") {
-    document.getElementById("theme-style").href = "default.css";
+    document.getElementById("theme-style").href = "css/default.css";
   }
   if (mode == "blue") {
-    document.getElementById("theme-style").href = "blue.css";
+    document.getElementById("theme-style").href = "css/blue.css";
   }
   if (mode == "green") {
-    document.getElementById("theme-style").href = "green.css";
+    document.getElementById("theme-style").href = "css/green.css";
   }
   if (mode == "purple") {
-    document.getElementById("theme-style").href = "purple.css";
+    document.getElementById("theme-style").href = "css/purple.css";
   }
 
   localStorage.setItem("theme", mode);
